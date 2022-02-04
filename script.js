f55cd45d0c2355d1f9eabd6e01dd97ac5745ef3a
Victoria <victoriyasotscaya2050@gmail.com>
2022-02-04 19:54:46 +0200

    added code to script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+"use strict";
+
+//Task1
+let name = prompt("Как тебя зовут?");
+alert(name);
+
+//Task2
+let x = 10;
+let y = 2;
+
+let sum = x + y;
+alert(sum);
+
+let subtraction = x - y;
+alert(subtraction);
+
+let multiplication = x * y;
+alert(multiplication);
+
+let division = x / y;
+alert(division);
+
+let exponentiation = x ** y;
+alert(exponentiation);
